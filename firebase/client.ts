e22941f5dde9867a808b42c42ed8db6dc4089d61
kodaishiotsuki <0711dr5@gmail.com>
2022-08-02 17:59:52 +0900

    環境変数

--- firebase/client.ts
+++ firebase/client.ts
@@ -5,17 +5,17 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADwkU4pWckCDz3R69rznh_Hawx2hUbv98",
-  authDomain: "dev-next-algolia.firebaseapp.com",
-  projectId: "dev-next-algolia",
-  storageBucket: "dev-next-algolia.appspot.com",
-  messagingSenderId: "1003217888075",
-  appId: "1:1003217888075:web:4e75f8b04f8d7b95a6fece",
-  measurementId: "G-QFC5ZMVKFE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-if(!getApps()?.length){
+if (!getApps()?.length) {
   initializeApp(firebaseConfig);
 }
 
